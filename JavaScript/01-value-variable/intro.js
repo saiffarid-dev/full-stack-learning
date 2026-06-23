f775ac56578dd12774c8a0078812f3c018ca2596
Saif <saif.faridi20@gmail.com>
2026-06-23 18:00:37 +0530

    Added 01 value variable files

---
+++ JavaScript/01-value-variable/intro.js
@@ -0,0 +1,26 @@
+//=====VALUE & VARIABLE NOTES====//
+// Value:
+// Any data stored in JavaScript. 
+// Example. 10, "Hello ", true
+// Variable .
+// syntax:
+// let variableName = value;
+
+// Example:
+let age = 20;
+let name = "Saif";
+
+// print method output receive
+console.log(age);
+console.log(name);
+
+// Here : age and name = Variables 
+//  20 and "Saif" = Values 
+// We use variables to store and reuse data.
+
+// Note : Value = Actual data
+// Variable = Container that stores data
+
+// exp: let city = "Delhi";
+// city -> Variable
+// Delhi -> Value
