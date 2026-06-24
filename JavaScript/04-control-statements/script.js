c028bd78ab6f59c3d9de75f3c82ea21aafd619c2
Saif <saif.faridi20@gmail.com>
2026-06-24 17:03:36 +0530

    New file or new codes notes updates

---
+++ JavaScript/04-control-statements/script.js
@@ -0,0 +1,75 @@
+// Simple If question
+
+// let age = 20;
+// if  (age >= 18) {
+//     console.log("You can vote");
+// }  
+
+// if Else
+
+// let age = 16;
+// if (age >= 18) {
+//     console.log("Adult");
+// }
+// else {
+//     console.log("Minor");
+// }
+
+// Even Odd
+
+// let number = 7;
+// if (number % 2 === 0) {
+//     console.log("Even");
+// }
+// else {
+//     console.log("Odd");
+// }
+
+// positive Negative
+
+// let number = -10;
+// if (number > 0) {
+//     console.log("Positive");
+// }
+// else {
+//     console.log("Negative");
+// }
+
+// Else If
+
+let marks = 85;
+if (marks >= 90) {
+    console.log("A Grade");
+}
+else if (marks >= 70) {
+    console.log("B Grade");
+}
+else if (marks >= 40) {
+    console.log("C Grade");
+}
+else {
+    console.log("Fail");
+}
+
+// pratice 6 password check
+
+let password = "admin123";
+if (password === "admin123") {
+    console.log("Login Success");
+}
+else {
+    console.log("Wrong Password");
+}
+
+// pratice 7 salary
+
+let salary = 25000;
+if (salary >= 30000) {
+    console.log("Eligible");
+} 
+else{
+    console.log("Not Eligible");
+}
+ 
+
+
