c17f08dc9a29ca3df53c0f2c4f114a4544130bc3
Saif <saif.faridi20@gmail.com>
2026-06-24 14:55:03 +0530

    New code or folder update

---
+++ JavaScript/03-operators/script.js
@@ -0,0 +1,101 @@
+
+// Arithmetic Operators
+
+let a = 10;
+let b = 20;
+
+console.log(a + b); // Addition
+console.log(a - b); // Subtraction
+console.log(a * b); // Multiplication
+console.log(a / b); // division
+console.log(a % b); // Reminder
+console.log(a ** 2); // power
+
+// 2 Assignment Operators
+
+// variable ki value update this is a question
+
+let x = 10;
+x += 5;
+console.log(x);
+
+x -= 3;
+console.log(x);
+
+x *= 2;
+console.log(x);
+
+x /= 4;
+console.log(x);
+
+// 3 Comparison Operators
+
+// two value compare
+
+console.log(10 == "10");
+console.log(10 === "10");
+console.log(10 != 5);
+console.log(10 > 5);
+console.log(10 < 5);
+console.log(10 >= 10);
+console.log(10 <= 10);
+
+
+// logical Operators
+
+// conditions combinte
+
+console.log(true && true);
+console.log(true && false);
+
+console.log(true || false);
+console.log(false || false);
+
+console.log(!true);
+console.log(!false);
+
+//  string operators
+
+let firstName = "Saif";
+let lastName = "Farid";
+
+console.log(firstName + " " + lastName);
+
+// 6 Unary Operators
+
+// value ko increase aur decrease karo
+
+let count = 5;
+
+count++;
+console.log(count);
+
+count--;
+console.log(count);
+
+// 7 Ternary Operator
+
+//  age check
+
+let age = 20;
+
+let result = age >= 18 ? "Adult" : "Minor";
+
+console.log(result);
+
+// 8 Type of Operators
+// data types check karo
+
+console.log(typeof "Saif");
+console.log(typeof 20);
+console.log(typeof true);
+
+// -->Final Pratice
+let name = "Saif";
+let marks = 85;
+console.log("Student: " + name);
+console.log("Marks: " + marks);
+
+console.log(marks >= 40 ? "Pass" : "Fail");
+console.log(typeof name);
+console.log(typeof marks);
