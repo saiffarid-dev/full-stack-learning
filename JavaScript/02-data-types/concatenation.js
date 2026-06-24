674fa604429e4062896c636e722a96b9e224e16f
Saif <saif.faridi20@gmail.com>
2026-06-24 13:32:50 +0530

    update new file

---
+++ JavaScript/02-data-types/concatenation.js
@@ -0,0 +1,33 @@
+//  question paratice Concatenation
+
+let firstName = "saif";
+let lastName = "Faird";
+console.log(firstName + lastName);
+
+// Name k bich space add kro.
+let Name1 = "Saif";
+let Name2 = "Ali";
+console.log(Name1 + " " + Name2);
+
+// User Ko Greeting Message do.
+
+let name = "Saif";
+console.log("Hello " + name);
+
+// Age dispaly kre
+
+let age = 20;
+console.log("My age is " + age);
+
+console.log("10" + 5);  // Number + String = string
+
+console.log("Java"  +  "Script");
+
+let Name = "Saif";
+let city = "Patna";
+let Age = 20;
+
+console.log("My Name is  " + name );
+console.log("I live in " + city);
+console.log("I am " + Age + "years old");
+
