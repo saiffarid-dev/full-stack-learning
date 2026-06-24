a9d8cbc288425bfee8a7aad068a4bd465c87059c
Saif <saif.faridi20@gmail.com>
2026-06-24 15:56:23 +0530

    New file or code updates

--- JavaScript/01-value-variable/intro.js
+++ JavaScript/01-value-variable/intro.js
@@ -7,12 +7,12 @@
 // let variableName = value;
 
 // Example:
-let age = 20;
-let name = "Saif";
+// let age = 20;
+// let name = "Saif";
 
 // print method output receive
-console.log(age);
-console.log(name);
+// console.log(age);
+// console.log(name);/
 
 // Here : age and name = Variables 
 //  20 and "Saif" = Values 
@@ -24,3 +24,43 @@ console.log(name);
 // exp: let city = "Delhi";
 // city -> Variable
 // Delhi -> Value
+
+// Print name
+console.log("Prince");
+
+// var-->
+var name = "Saif";
+console.log(name);
+
+// Var Redeclaration 
+var name = "Saif";
+var name = "Farid";
+console.log(name);
+
+// let -->
+// Change the value of a varible
+let age = 20;
+age = 21;
+console.log(age);
+
+// let Redecaration -->
+let city = "Patna";
+// let city = "Delhi";
+console.log(city);
+
+// const --->
+const country = "India";
+console.log(country);
+
+// const Reassignment 
+// const country = "India";
+// country = "USA";
+
+// console.log(country);  // ye error aaya islye comment kr rhe hai
+
+
+const company = "Google";
+let score = 0;
+score = 10;
+console.log(company);
+console.log(score);
