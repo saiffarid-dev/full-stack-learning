dd482d737a13e659e05dd75c7a1e89e77ac6c834
Saif <saif.faridi20@gmail.com>
2026-07-02 15:27:26 +0530

    new folder create new codes writes

---
+++ JavaScript/10-strings/script.js
@@ -0,0 +1,35 @@
+// ============ String Introduction ============
+
+
+{
+    // Double   Quotes
+
+    let name = "Raju";
+
+    // Single Quotes
+
+    let course = 'JavaScript';
+
+    // Template Literal (Backticks)
+
+    let message = `Welcome to JavaScript`;
+
+    console.log(name);
+    console.log(course);
+    console.log(message);
+
+    console.log(typeof name);
+    console.log(typeof course);
+    console.log(typeof message);   //  this is used for check data types.. //
+}
+
+
+// ======= string Length ========== >>>>>
+
+{
+    let text = "JavaScript";
+    console.log(text);
+    console.log(text.length);
+    console.log(text[0]);
+    console.log(text[text.length-1]);
+}
