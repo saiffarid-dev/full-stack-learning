09fedac03fb7ec4c5e2928d6ab4a6814594c59cb
Saif <saif.faridi20@gmail.com>
2026-07-18 16:28:35 +0530

    Add new python pratice variable

---
+++ Python-Practice/01_Variables/01_basic.py
@@ -0,0 +1,15 @@
+name = "Saif"
+age = 25
+city = "Delhi"
+college =  "Jain College"
+mobile = "Redmi note 12"
+language =  "Python"
+score = 40
+score = 100
+
+print(name, age, city, college, mobile, language, score)
+
+name1 = "Ali"
+Age = 26
+city1 = "Dubai"
+print ("my name is name1" )
