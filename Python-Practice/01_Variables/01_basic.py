b414601e627ae45c1af9cc86e28f010aec70abac
Saif <saif.faridi20@gmail.com>
2026-07-20 16:54:22 +0530

    my paratice codes

--- Python-Practice/01_Variables/01_basic.py
+++ Python-Practice/01_Variables/01_basic.py
@@ -1,15 +1,87 @@
-name = "Saif"
-age = 25
+# name = "Saif"
+# age = 25
+# city = "Delhi"
+# college =  "Jain College"
+# mobile = "Redmi note 12"
+# language =  "Python"
+# score = 40
+# score = 100
+
+# print(name, age, city, college, mobile, language, score)
+
+# name1 = "Ali"
+# Age = 26
+# city1 = "Dubai"
+# print ("my name is name1" )
+
+
+# ============ new coding ========
+
+
+
+name = "saif"
+print(name)
 city = "Delhi"
-college =  "Jain College"
-mobile = "Redmi note 12"
-language =  "Python"
+print(city)
+
+name = "Ali"
+age = 25
+print(name)
+print(age)
+
+name  = "Prince"
+age = 26
+city = "Delhi"
+
+print(name, age, city)
+
 score = 40
+print(score)
+
 score = 100
+print(score)
 
-print(name, age, city, college, mobile, language, score)
+name = "Saif"
+print("My name is", name)
 
-name1 = "Ali"
-Age = 26
-city1 = "Dubai"
-print ("my name is name1" )
+age = 26 
+print("I am", age, "years old")
+
+city = "Delhi"
+print("I live in", city)
+
+name = "Saif"
+age = 26
+city = "Delhi"
+print("Name :", name)
+print("Age :", age)
+print("City :", city)
+
+a, b, c = 10, 20, 30
+print(a)
+print(b)
+print(c)
+
+x = y = z = 100
+print(x)
+print(y)
+print(z)
+
+# SWAP Variables
+
+a = 10 
+b = 20
+a, b = b, a
+print(a)
+print(b)
+
+first_name = "Saif"
+last_name = "Farid"
+
+print(first_name, last_name)
+
+college = "Jain College"
+course = "MCA"
+
+print("College :", college)
+print("Course :", course)
